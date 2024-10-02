8b9420bd8449aac74795df6cc70c0192a947bc52
Najja Kossally <35144945+nkossally@users.noreply.github.com>
2024-10-02 05:37:57 -0400

    modify comment

--- src/components/Hand.js
+++ src/components/Hand.js
@@ -11,9 +11,9 @@ const Hand = () => {
         return (
           <Letter
             letter={letter}
-            // Add random number to the key in order to prevent a glitch in which,
-            // when a tile is dragged and dropped, another tile with the same letter
-            // is not also moved.
+            // Add a random number to the key in order to prevent a glitch.
+            // Without the random number, when a tile is dragged and dropped, 
+            // another tile with the same letter is also moved.
             key={`draggable-${i}.${letter}.${Math.random()}`}
             handIdx={i}
             isInHand={true}
